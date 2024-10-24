800f53ba58d35efa9cba2825fdcf916dc6e9b0d1
HusseinCoulibaly <GnenemaCoulibaly@teccart.online>
2024-10-23 22:42:58 -0400

    probleme de connexion a mongodb atlas

--- biometrics-service/server.js
+++ biometrics-service/server.js
@@ -8,8 +8,7 @@ connectDB();
 
 app.use(express.json());
 
-// Routes pour Google Fit
-app.use('/api/googlefit', require('./routes/googleFitRoutes'));
+
 
 const PORT = process.env.PORT || 5002;
-app.listen(PORT, () => console.log(`Google Fit Service running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Service running on port ${PORT}`));
